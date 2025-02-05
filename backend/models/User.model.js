6a8eda6d5c35aa151a1abe3ae87d6f1f280fb3f4
Nihad <mncnihad@gmail.com>
2025-02-05 10:46:59 +0530

    Mongoose User Model Schema Created

---
+++ backend/models/User.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    email_verify: {
+      type: Boolean,
+      default: false,
+    },
+    employee_verify: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpire: {
+      type: Date,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
