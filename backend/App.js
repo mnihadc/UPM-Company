70940efc9dcd7605087976d8fab681065f84dd1e
Nihad <mncnihad@gmail.com>
2025-02-05 09:23:48 +0530

    Created Mongodb Atlas and connected Mongoose

--- backend/App.js
+++ backend/App.js
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
 app.listen(port, () => {
   console.log(`Server is Running on port ${port}!!!`);
 });
